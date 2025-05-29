25d7faf3dec08a5ba4268cb5ac08d3e25e28585d
bravoagustin <bravoagustin149@gmail.com>
2025-05-28 22:48:36 -0300

    Concat String

---
+++ src/main/java/ConcatenacionDeCadenas.java
@@ -0,0 +1,21 @@
+public class ConcatenacionDeCadenas {
+    public static void main(String[] args) {
+        var cadena1 = "Hola";
+        var cadena2 = "Mundo";
+
+        var cadena3 = cadena1 + " " + cadena2;
+        System.out.println("cadena3 = " + cadena3);
+
+        //Metodo Concat
+        cadena3 = cadena1.concat(" ").concat("Mundo");
+        System.out.println("cadena3 = " + cadena3);
+
+        //StringBuilder new es clase
+        var constructorCadenas = new StringBuilder();
+        constructorCadenas.append(cadena1);
+        constructorCadenas.append(" ");
+        constructorCadenas.append(cadena2);
+        var resultado = constructorCadenas.toString();
+        System.out.println("resultado = " + resultado);
+    }
+}
